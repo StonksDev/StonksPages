7c2776a598341b335836858ce8cc447acc290f67
stonksrokit <keyboardkatsu@gmail.com>
2024-12-18 22:04:16 +0100

    ui polishing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-    for (let i = 0; i < 42; i++) {
+    for (let i = 0; i < 69; i++) {
         const colors = ['red', 'green', 'blue', 'orange', 'purple', 'pink', 'yellow', 'cyan', 'lime'];
         const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        console.log(`%cstonks`, `color: ${randomColor}; font-weight: bold; font-size: 16px;`);
+        console.log(`%cSTONKS`, `color: ${randomColor}; font-weight: bold; font-size: 16px;`);
     }
